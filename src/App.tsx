eb1bb5ae1ac72b0e3cf659ea8dd076441dae1148
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-09-16 13:31:43 -0400

    Scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import Container from 'react-bootstrap/Container';
-import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
+import { Outlet, Route, Routes } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Home } from './pages/Home';
+import { QrCodePage } from './pages/QrCodePage';
 
 function App() {
   return (
-    <Navbar>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="qrcode" element={<QrCodePage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Utils</Navbar.Brand>
+        <Navbar.Brand href="/">Utils</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <LinkContainer to="/qrcode">
+              <Nav.Link>QR Code Generator</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <Container className="p-3">
+      <Outlet />
+    </Container>
+    </>
   );
 }
 
---
+++ src/pages/QrCodePage.tsx
@@ -0,0 +1,3 @@
+export function QrCodePage() {
+  return (<p>ABOUT</p>);
+}
