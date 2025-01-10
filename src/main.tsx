d06a8c73ca12661776307347e35c3bb55ab6e55c
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-01-10 16:57:40 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const updateTheme = () => document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
+window.addEventListener('DOMContentLoaded', updateTheme);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
