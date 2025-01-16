ef89257fa9cf63243ea1f05a53feb47378070789
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-01-16 13:12:18 -0500

    Add base64 conversion support

--- src/pages/TextUtilsPage.tsx
+++ src/pages/TextUtilsPage.tsx
@@ -19,6 +19,8 @@ export function TextUtilsPage() {
   const formatJson = () => updateOutput(() => JSON.stringify(JSON.parse(input), null, 2));
   const urlEncode = () => updateOutput(() => encodeURIComponent(input));
   const urlDecode = () => updateOutput(() => decodeURIComponent(input));
+  const base64Encode = () => updateOutput(() => btoa(input));
+  const base64Decode = () => updateOutput(() => atob(input));
   const lineCount = input.split(/\n+/).length;
   const wordCount = input.split(/\s+/).length;
   const charCount = input.length;
@@ -54,6 +56,8 @@ export function TextUtilsPage() {
       }
       <Form.Group className="mb-3">
         <Button className="ms-1" type="button" onClick={formatJson}>Pretty JSON</Button>
+        <Button className="ms-1" type="button" onClick={base64Encode}>Base64 Encode</Button>
+        <Button className="ms-1" type="button" onClick={base64Decode}>Base64 Decode</Button>
         <Button className="ms-1" type="button" onClick={urlEncode}>URL Encode</Button>
         <Button className="ms-1" type="button" onClick={urlDecode}>URL Decode</Button>
       </Form.Group>
