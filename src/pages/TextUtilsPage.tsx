bcd5a8b31e137eb77078d10e57ca930fea86131c
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-02-19 21:32:17 -0500

    Support YAML and JSON conversion

--- src/pages/TextUtilsPage.tsx
+++ src/pages/TextUtilsPage.tsx
@@ -1,5 +1,6 @@
 import { Alert, Button, Col, Container, Form, Row, Table } from "react-bootstrap";
 import { useState } from "react";
+import * as yaml from "yaml";
 
 const splitLines = (text: string) => text.split(/\r?\n/);
 const joinLines = (lines: string[]) => lines.join('\n');
@@ -30,6 +31,8 @@ export function TextUtilsPage() {
   const base64Decode = () => updateOutput(() => atob(input));
   const removeDuplicateLines = () => updateOutputLines(input, lines => lines.filter((line, index, lines) => lines.indexOf(line) === index));
   const sortLines = () => updateOutputLines(input, lines => lines.sort());
+  const yamlToJson = () => updateOutput(() => JSON.stringify(yaml.parse(input), null, 2));
+  const jsonToYaml = () => updateOutput(() => yaml.stringify(JSON.parse(input), null, 2));
   const lineCount = input.split(/\n+/).length;
   const wordCount = input.split(/\s+/).length;
   const charCount = input.length;
@@ -71,6 +74,8 @@ export function TextUtilsPage() {
         <Button className="ms-1" type="button" onClick={base64Decode}>Base64 Decode</Button>
         <Button className="ms-1" type="button" onClick={urlEncode}>URL Encode</Button>
         <Button className="ms-1" type="button" onClick={urlDecode}>URL Decode</Button>
+        <Button className="ms-1" type="button" onClick={yamlToJson}>YAML to JSON</Button>
+        <Button className="ms-1" type="button" onClick={jsonToYaml}>JSON to YAML</Button>
       </Form.Group>
       <Form.Group className="mb-3">
         <Table>
